b965240a01d2d78cfc3f5f822ac28d0be996c144
cayman718 <a44362652@gmail.com>
2024-11-28 09:51:52 +0800

    js教學

---
+++ 2024-11-28/test.php
@@ -0,0 +1,15 @@
+<?php
+$name = 'a-kai';
+echo ("Hi $name");
+
+// array
+
+// index array
+$indexArr = [1, 2, 3];
+
+// Associative array
+$assocArr = [
+    's1' => 'amy',
+    's2' => 'bob',
+    's3' => 'cat'
+];
